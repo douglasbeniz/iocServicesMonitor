2255289bb07b1f99ab3ae7b17a5827ed34cd2c2b
Douglas Beniz <douglasbeniz@yahoo.com.br>
2019-06-25 18:18:40 +0200

    Updating ics.iocmach.01 IP back to cslab net;

--- config.py
+++ config.py
@@ -11,6 +11,6 @@
 class Config(object):
     SERVERS = { 'ics-iocsad-01':'10.4.3.221',
         'ics-banana-evr2.cslab.esss.lu.se':'172.30.242.12',
-        'ics-iocmach-01.cslab.esss.lu.se':'10.4.0.222',
+        'ics-iocmach-01.cslab.esss.lu.se':'172.30.244.12',
         'det-iocmach-01.cslab.esss.lu.se':'172.30.244.21' }
 
